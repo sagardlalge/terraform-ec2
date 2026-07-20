22ff73b62b751aaab86aecef6e3503702577efd4
Sagar Lalge <85780241+sagardlalge@users.noreply.github.com>
2026-07-20 16:27:21 +0530

    Add AWS EC2 instance resource configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
 
+  tags = {
+    Name        = "jenkins-created-ec2"
+    Environment = "Dev"
+    Terraform   = "True"
+  }
+}
